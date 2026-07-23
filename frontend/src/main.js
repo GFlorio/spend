6da6e77369ba4cb14fc04aefe22d1ab84846261a
Gabriel Florio <gabriel@gabrielflorio.com>
2026-07-23 15:15:39 -0300

    ui: envelopes overview and transaction-history detail screen

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,12 @@
-import { setupUI } from './ui/ui.js';
+import { renderEnvelopes, setupEnvelopes } from './ui/envelopes.js';
 import { openInitialMonth, setupMonth } from './ui/month.js';
+import { onEnvelopesShown, setupUI } from './ui/ui.js';
 
 void (async function init() {
   setupUI();
   setupMonth();
+  setupEnvelopes();
+  onEnvelopesShown(async () => { await renderEnvelopes(); });
   await openInitialMonth();
+  await renderEnvelopes();
 })();
